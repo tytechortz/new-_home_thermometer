77b647f79a48cad85f06c79d01f9c48ff3589913
James <tytechortz@gmail.com>
2022-07-10 15:22:54 -0600

    adds live thermomter

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
+import requests
+
 import pandas as pd
 
 url = "http://10.0.1.7:8080"
@@ -13,9 +16,29 @@ application = app.server
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
-    html.H1('Hello')
-
+    html.Div([
+        html.Div([
+            html.Div(id='live-thermometer', style={'color':'green', 'font-size': 40, 'font-family':'sans-serif'})
+        ],
+            className='four columns'
+        ),
+    ],
+        className='row'
+    ),
+    dcc.Interval(
+                    id='interval-component',
+                    interval=60000,
+                    n_intervals=0
+                ),
 ])
+
+@app.callback(Output('live-thermometer', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_layout(n):
+    res = requests.get(url)
+    data = res.json()
+    f = ((9.0/5.0) * data) + 32
+    return 'Current Temperature: {:.1f}'.format(f)
 
 if __name__ == "__main__":
     app.run_server(port=8000, debug=True)
